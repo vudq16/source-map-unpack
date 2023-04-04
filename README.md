43e52c8c0e197ed262f4b712c8802e33136945a5
supernaruto16 <supernaruto16@gmail.com>
2023-04-04 17:45:02 +0700

    update

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 [![npm version](https://badge.fury.io/js/source-map-unpack.svg)](https://badge.fury.io/js/source-map-unpack)
 [![Twitter](https://img.shields.io/twitter/url/https/www.npmjs.com/package/source-map-unpack.svg?style=social)](https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fwww.npmjs.com%2Fpackage%2Fsource-map-unpack)
 
-### CLI to unpack 🛍  your JS source maps 🗺  to original files and folders.
+### CLI to unpack 🛍 your JS source maps 🗺 to original files and folders.
 
 ### Usage:
-1. ``` npm install -g source-map-unpack ```
-2. ``` unpack <project-folder> <path-to-sourcemap> ```
+
+1. `npm install -g source-map-unpack`
+2. `unpack <project-folder> <path-to-sourcemap>`
 
 For example:
 
-``` unpack egghead js/egghead-bundle.js.map ```
+`unpack egghead js/egghead-bundle.js.map`
 
 Note:
 
 The original minified file should be placed according to the path in sources map's `file` variable.
-
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,46 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
-import mkdirp from 'mkdirp';
+import * as mkdirp from 'mkdirp';
 import * as chalk from 'chalk';
 import * as minimist from 'minimist';
 import { SourceMapConsumer } from 'source-map';
-import { dirname, join, isAbsolute } from 'path';
+import * as path from 'path';
+
+const unpack = (pathToProject: string, pathToMap: string) => {
+  try {
+    const mapFile = fs.readFileSync(pathToMap, 'utf8');
+    console.log(chalk.green(`[+] Unpacking source map: ${pathToMap}`));
+    SourceMapConsumer.with(mapFile, null, (consumer: SourceMapConsumer) => {
+      const sources = (consumer as any).sources;
+      sources.forEach((source: string) => {
+        const WEBPACK_SUBSTRING_INDEX = 11;
+        const content = consumer.sourceContentFor(source) as string;
+        const filePath = path.resolve(
+          process.cwd(),
+          `${pathToProject}/${source.substring(WEBPACK_SUBSTRING_INDEX)}`,
+        );
+        try {
+          mkdirp.sync(path.dirname(filePath));
+        } catch (e) {}
+        fs.writeFileSync(filePath, content);
+      });
+    });
+  } catch (err) {
+    console.log(chalk.red('[!] Something is wrong with the source map'));
+    // console.log('\n', err);
+    // process.exit(1);
+  }
+};
 
 const argv = minimist(process.argv.slice(2));
-const projectNameInput = argv._[0];
-const mapInput = argv._[1];
+const mapInput = argv._[0];
+const projectNameInput = argv._[1];
 
 if (!projectNameInput || !mapInput) {
   console.log();
   console.log(
     chalk.white('Usage: unpack'),
-    chalk.green('<project-directory> <path-to-map-file>'),
+    chalk.green('<map-files-dir> <project-dir>'),
   );
   console.log();
   console.log(
@@ -26,48 +52,28 @@ if (!projectNameInput || !mapInput) {
   process.exit();
 }
 
-const pathToProject = join(process.cwd(), projectNameInput);
-const pathToMap = isAbsolute(mapInput)
+const pathToProject = path.resolve(process.cwd(), projectNameInput);
+const pathToMapsDir = path.isAbsolute(mapInput)
   ? mapInput
-  : join(process.cwd(), mapInput);
+  : path.resolve(process.cwd(), mapInput);
 
-if (fs.existsSync(pathToProject)) {
-  console.log();
-  console.log(chalk.red(`Project folder already exists at: ${pathToProject}`));
-  console.log();
-  process.exit(1);
-}
+const stack: string[] = [pathToMapsDir];
 
-if (!fs.existsSync(pathToMap)) {
-  console.log();
-  console.log(chalk.red(`Can't find map file under : ${pathToMap}`));
-  console.log();
-  process.exit(1);
-}
+while (stack.length > 0) {
+  const currentDir = stack.pop()!;
+  const files = fs.readdirSync(currentDir);
 
-try {
-  const mapFile = fs.readFileSync(pathToMap, 'utf8');
-  SourceMapConsumer.with(mapFile, null, (consumer: SourceMapConsumer) => {
-    console.log(chalk.green(`Unpacking 🛍  your source maps 🗺`));
-    const sources = (consumer as any).sources;
-    sources.forEach((source: string) => {
-      const WEBPACK_SUBSTRING_INDEX = 11;
-      const content = consumer.sourceContentFor(source) as string;
-      const filePath = `${process.cwd()}/${projectNameInput}/${source.substring(
-        WEBPACK_SUBSTRING_INDEX,
-      )}`;
-      mkdirp.sync(dirname(filePath));
-      fs.writeFileSync(filePath, content);
-    });
-    console.log(chalk.green('🎉  All done! Enjoy exploring your code 💻'));
-  });
-} catch (err) {
-  console.log(chalk.red('Oops! Something is wrong with the source map'));
-  console.log(
-    chalk.red(
-      'Make sure .min.js is correctly placed under the path specified in .map file',
-    ),
-  );
-  console.log('\n', err);
-  process.exit(1);
+  for (const file of files) {
+    const filePath = path.resolve(currentDir, file);
+    const fileStat = fs.statSync(filePath);
+
+    if (fileStat.isDirectory()) {
+      stack.push(filePath);
+    } else {
+      if (file.endsWith('.map')) {
+        // console.log(filePath);
+        unpack(pathToProject, filePath);
+      }
+    }
+  }
 }
